56339b2e709d0e1d127971bcee176f4b300f8a24
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-02-15 06:43:23 +0530

    Create test.h
    test.h is created

---
+++ test.h
@@ -0,0 +1,8 @@
+//function to test the functions
+void test_functions(void);
+
+//fucntion to test sensor read
+void test_sensorRead(void);
+
+//function to test sensor data send
+void test_sender(void);
