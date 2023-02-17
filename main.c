f3baea820884b72bd65af5f68cfe080bc743db07
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-02-17 15:23:57 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,31 +6,12 @@
 #include "file.h"
 #include "test.h"
 
-/*int main(int argc, char **argv)
-{
-    if(argc == 1)
-    {
-        printf("Usage: %s [-t]/[-r]\n[-t]: To run the unit test cases\n[-r]: To send the sensors data via console output\n", argv[0]);
-        exit(0);
-    }
-
-    if(strcmp(argv[1],"-t") == 0)
-    {
-        test_functions();
-    }
-    else if(strcmp(argv[1],"-r") == 0)
-    {
-        sendSensorValue(&printToConsole);
-    }
-
-    return 0;
-}*/
-
 void correctUsageInfoAndExit(char* exeName)
 {
     printf("Usage: %s [-t]/[-r]\n[-t]: To run the unit test cases\n[-r]: To send the sensors data via console output\n", exeName);
     exit(0);
 }
+
 int main(int argc, char **argv)
 {
     if(argc == 1)
