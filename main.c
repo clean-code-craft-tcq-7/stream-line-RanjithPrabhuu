03e5629c99f18279ccb1fc7f79d2133b0498a3ba
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-02-17 15:20:43 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "file.h"
 #include "test.h"
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
     if(argc == 1)
     {
@@ -21,6 +21,34 @@ int main(int argc, char **argv)
     else if(strcmp(argv[1],"-r") == 0)
     {
         sendSensorValue(&printToConsole);
+    }
+
+    return 0;
+}*/
+
+void correctUsageInfoAndExit(char* exeName)
+{
+    printf("Usage: %s [-t]/[-r]\n[-t]: To run the unit test cases\n[-r]: To send the sensors data via console output\n", exeName);
+    exit(0);
+}
+int main(int argc, char **argv)
+{
+    if(argc == 1)
+    {
+        correctUsageInfoAndExit(argv[0]);
+    }
+
+    if(strcmp(argv[1],"-t") == 0)
+    {
+        test_functions();
+    }
+    else if(strcmp(argv[1],"-r") == 0)
+    {
+        sendSensorData(&printToConsole);
+    }
+    else
+    {
+        correctUsageInfoAndExit(argv[0]);
     }
 
     return 0;
